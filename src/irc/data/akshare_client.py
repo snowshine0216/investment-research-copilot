42fb6e6b9894aaa7583b925a334d51df8f58e50d
Xue Yin <snowshine@163.com>
2026-07-02 12:28:05 +0800

    refactor(monitor): akshare_client._proxy_env delegates to irc.http_proxy.proxy_env

--- src/irc/data/akshare_client.py
+++ src/irc/data/akshare_client.py
@@ -5,14 +5,14 @@ import contextlib
 from datetime import date
 from functools import lru_cache
 import logging
-import os
 import re
 import threading
 import time
-from typing import Any, Generator, TypeVar
+from typing import Any, TypeVar
 
 import pandas as pd
 
+from irc.http_proxy import proxy_env as _proxy_env
 from irc.http_proxy import resolve_proxy
 
 _log = logging.getLogger(__name__)
@@ -32,24 +32,6 @@ def _ak_call(fn_name: str, **kwargs: Any) -> Any:
     import akshare as ak  # local import
     fn = getattr(ak, fn_name)
     return fn(**kwargs)
-
-
-@contextlib.contextmanager
-def _proxy_env(proxy_url: str) -> Generator[None, None, None]:
-    """Temporarily inject HTTP/HTTPS proxy env vars so requests-based libs
-    (akshare, etc.) route through the proxy. Restores original values on exit."""
-    keys = ("HTTP_PROXY", "HTTPS_PROXY", "http_proxy", "https_proxy")
-    saved = {k: os.environ.get(k) for k in keys}
-    for k in keys:
-        os.environ[k] = proxy_url
-    try:
-        yield
-    finally:
-        for k, v in saved.items():
-            if v is None:
-                os.environ.pop(k, None)
-            else:
-                os.environ[k] = v
 
 
 def _sleep(seconds: float) -> None:
